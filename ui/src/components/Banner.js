499c509b6d28ed3939bba339e4c30531bf640d68
kanetah <2205485120@qq.com>
2018-02-09 18:50:43 +0800

    redo ui module

---
+++ ui/src/components/Banner.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+export default class Banner extends Component {
+    render() {
+        return (
+            <div
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    background: "red",
+                }}
+            >
+            </div>
+        )
+    }
+}
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
 import './style/index.css';
 import App from './farme/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
