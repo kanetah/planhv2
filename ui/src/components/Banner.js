ad8f7b96f5da21924f81f8c90b0b587b453feb17
kanetah <2205485120@qq.com>
2018-02-10 23:20:22 +0800

    add banner

--- ui/src/components/Banner.js
+++ ui/src/components/Banner.js
@@ -1,15 +1,42 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import banner from '../assets/banner.jpg';
 
 export default class Banner extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            zIndex: 10,
+        }
+    }
+
+    onMouseMove = e => {
+        this.setState({
+            zIndex: -1,
+        });
+        console.log(e);
+    };
+
     render() {
         return (
-            <div
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    background: "red",
-                }}
-            >
+            <div onMouseMove={this.onMouseMove} style={{
+                width: "100%",
+                height: "100%",
+                background: `url(${banner}) no-repeat`,
+                backgroundSize: "cover",
+                color: "white",
+                display: "flex",
+                zIndex: this.state.zIndex,
+            }}>
+                <div style={{
+                    width: "auto",
+                    height: "auto",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}>
+                    <h1>PlanH V2</h1>
+                    <h3>厦理软15移春2班的作业提交平台</h3>
+                </div>
             </div>
         )
     }
